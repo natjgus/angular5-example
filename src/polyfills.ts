8e8f968a8fc376ee63de6ce3dfbe8b3db3f49156
Nathaniel Gustafson <natjgus@gmail.com>
2018-02-26 16:15:39 -0700

    added polyfills

---
+++ src/polyfills.ts
@@ -0,0 +1,6 @@
+// Polyfills are system requirements which we copied from angular
+//If you don't load polyfills first angular does NOT work
+
+import "core-js/es6";
+import "core-js/es7/reflect";
+import "zone.js/dist/zone";
