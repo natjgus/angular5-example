14a37ef9c9eed4a6f000b5b30b25f6b31ef6d425
Nathaniel Gustafson <natjgus@gmail.com>
2018-02-26 16:32:43 -0700

    added main entry point

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
 
+
+//this delays angular from loading ever so slightly to allow other things to load
 window.addEventListener("DOMContentLoaded", event => {
 	// enableProdMode();
 	const platform = platformBrowserDynamic();
