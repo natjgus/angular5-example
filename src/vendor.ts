ae82c8e638a5b0fbbe7a71cab732a3ea768af4ed
Nathaniel Gustafson <natjgus@gmail.com>
2018-02-26 16:16:55 -0700

    added vendor.ts

---
+++ src/vendor.ts
@@ -0,0 +1,19 @@
+//Thhis is just importing angular itself
+
+// Angular 5
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+import "@angular/core";
+import "@angular/common";
+import "@angular/router";
+
+// RxJS
+import "rxjs";
+
+// Other vendors for example Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+//You can import any other packages here
+import "jquery";
+import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "font-awesome-webpack2";
