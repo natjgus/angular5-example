ebb3804c5f423853c99bc02f36f6a4c13ab1d287
Nathaniel Gustafson <natjgus@gmail.com>
2018-02-26 16:41:47 -0700

    added component file

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+	selector: "angular5-example-app",
+	templateUrl: "./templates/angular5-example-app.html"
+})
+
+export class AppComponent {}
+
+
+
+//Components are important! They allow you to create your own tags
+//We are doing this here with the angular5-example-app selector on line 4
